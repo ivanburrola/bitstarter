e602256577496fd2ae5486dad058ee08dee31bd4
Ivan Burrola <ivan.burrola@gmail.com>
2013-07-11 22:08:58 +0000

    grader.js update to take either a file or a URL as an input on the command line

--- grader.js
+++ grader.js
@@ -24,8 +24,10 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require("restler");
 var HTMLFILE_DEFAULT = "index.html";
-var CHECKFILE_DEFAULT = "checks.json";
+var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = 'http://protected-fjord-1990.herokuapp.com/';
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -40,30 +42,65 @@ var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
 };
 
+var cheerioUrl = function(url) {
+    return cheerio.load(url);
+};
+
 var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtmlFile = function(htmlfile, checksfile) {
-    $ = cheerioHtmlFile(htmlfile);
+var checkHtmlFile = function(htmlfile, checksfile, file) {
+
+    $ = (file) ? cheerioHtmlFile(htmlfile) : cheerioUrl(htmlfile);
+
+
     var checks = loadChecks(checksfile).sort();
     var out = {};
-    for(var ii in checks) {
+    for(var ii in checks){
 	var present = $(checks[ii]).length > 0;
-	out[checks[ii]] = present;
-	    }
+			out[checks[ii]] = present;
+}		       
 	return out;
 };
 
+
+
+var clone = function (fn){
+    // Workaround for commander.js issue.
+    // http://stackoverflow.com/a/6772648
+    return fn.bind({});
+ 
+}
+
+
+
+
 if(require.main == module) {
     program
-    .option('-c, --checks ','Path to checks.json', assertFileExists, CHECKFILE_DEFAULT)
-    .option('-f, --file ', 'Path to index.html', assertFileExists, HTMLFILE_DEFAULT)
+    .option('-c, --checks <check_file> ','Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
+    .option('-f, --file <html_file> ', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+    .option('-u, --url  <url>' , 'URL to index.html')
     .parse(process.argv);
     
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+
+ 
+    if(program.url != undefined){
+
+
+	rest.get(program.url).on('complete', function(data){
+	    
+	 var checkJson = checkHtmlFile(data,program.checks, false);
+         var outJson = JSON.stringify(checkJson, null, 4);
+         console.log(outJson);
+  
+	});
+    } else {
+	 var checkJson = checkHtmlFile(program.file, program.checks, true);
+	 var outJson = JSON.stringify(checkJson, null, 4);
+	console.log(outJson);    
+    }
+
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
